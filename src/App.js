0386df2bcfcd12669d92a8e4c0c8703e03c15378
Maxbrunetti <brunetti.max14@gmail.com>
2023-07-28 19:11:08 +0200

    semantic tags

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import './App.css';
 /** @jsxImportSource react */
 
 function App() {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <meta name="og:title" content="Little Lemon restaurant" />
+      <meta name="og:description" content="serving Italian food since 2019" />
+      <meta name="og:image" content="" />
+      <header>Header</header>
+      <nav>
+        <img src="" alt="logo" />
+        <ul>
+          <li>EL1</li>
+          <li>EL2</li>
+          <li>EL3</li>
+          <li>EL4</li>
+        </ul>
+      </nav>
+      <main></main>
+      <footer></footer>
+    </>
+  );
 }
 
 export default App;
